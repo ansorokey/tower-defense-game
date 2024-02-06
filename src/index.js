b81fbe2d612010ed9cffd242ee1eede56905fe28
ansorokey <ansorokey@gmail.com>
2024-02-05 20:08:59 -0500

    enemies move along path from center instead of xy origin

--- src/index.js
+++ src/index.js
@@ -26,6 +26,10 @@ class Enemy {
         this.width = 100;
         this.height = 100;
         this.curWaypoint = 0;
+        this.center = {
+            x: this.position.x + this.width/2,
+            y: this.position.y + this.height/2
+        }
     }
 
     draw() {
@@ -38,27 +42,30 @@ class Enemy {
         );
     }
 
-    // draws this enemy and changes position every frame
     update() {
         this.draw();
 
         // this calculation finds the angle to the next waypoint using trig
         const waypoint = waypoints[this.curWaypoint];
-        const yDist = waypoint.y - this.position.y;
-        const xDist = waypoint.x - this.position.x;
+        const yDist = waypoint.y - this.center.y;
+        const xDist = waypoint.x - this.center.x;
         const angle = Math.atan2(yDist, xDist);
 
         // Rather than a static value,
         // we use the values above as the x and y velocity to move to the enxt point
         this.position.x += Math.cos(angle);
         this.position.y += Math.sin(angle);
+        this.center = {
+            x: this.position.x + this.width/2,
+            y: this.position.y + this.height/2
+        }
 
         if(
             // prevent out of bounds errors
             this.curWaypoint < waypoints.length - 1 &&
             // round both values to integers since they would be slightly off as floating points otherwise
-            Math.round(this.position.x) === Math.round(waypoint.x) &&
-            Math.round(this.position.y) === Math.round(waypoint.y)
+            Math.round(this.center.x) === Math.round(waypoint.x) &&
+            Math.round(this.center.y) === Math.round(waypoint.y)
         ) {
             this.curWaypoint += 1;
         }
@@ -66,7 +73,7 @@ class Enemy {
 }
 
 const enemy = new Enemy({
-    // sthat them outside the bounds of the map, at the first waypoint
+    // start them outside the bounds of the map, at the first waypoint
     position: {
         x:waypoints[0].x,
         y: waypoints[0].y

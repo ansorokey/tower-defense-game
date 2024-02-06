f6bfc2a093b67f9188ccfa2381ce750e7fea12f8
ansorokey <ansorokey@gmail.com>
2024-02-05 19:43:40 -0500

    basic enemy class created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { waypoints } from "./waypoints.js";
+
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
@@ -8,7 +10,56 @@ canvas.height = 768;
 // c.fillRect(0, 0, canvas.width, canvas.height);
 
 const mapImg = new Image();
-// the image may take a bit to load into JS,
-//so we use an onload function to draw it when loaded
-mapImg.onload = () => c.drawImage(mapImg, 0, 0)
 mapImg.src = '/assets/tilesets/map.png';
+// image loading is slightly async,
+//so we use an onload function to initiate
+mapImg.onload = () => {
+    animate();
+}
+
+// Enemy Class Definition
+class Enemy {
+    constructor({
+        position={x:0, y:0},
+    }) {
+        this.poisition = position;
+        this.width = 100;
+        this.height = 100;
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(
+            this.poisition.x,
+            this.poisition.y,
+            this.width,
+            this.height
+        );
+    }
+
+    // draws this enemy and changes position every frame
+    update() {
+        this.draw();
+        this.poisition.x += 1;
+    }
+}
+
+const enemy = new Enemy({
+    position: {x:200, y:400},
+});
+
+const enemy2 = new Enemy({
+    position: {x:100, y:400},
+});
+
+function animate() {
+    // the primary animation loop
+    window.requestAnimationFrame(animate);
+
+    // draw the background every frame
+    c.drawImage(mapImg, 0, 0)
+
+    // update the enemy
+    enemy.update();
+    enemy2.update();
+}

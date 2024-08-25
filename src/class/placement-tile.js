d9a770793b8a403a43a7c59b6dacc1b090702953
ansorokey <ansorokey@gmail.com>
2024-08-25 00:09:10 -0400

    test update

--- src/class/placement-tile.js
+++ src/class/placement-tile.js
@@ -48,4 +48,21 @@ export default class PlacementTile {
             this.color = 'rgba(255, 255, 255, 0.25)';
         }
     }
+
+    // Test update
+    // update(mouse) {
+    //     this.draw()
+
+    //     // check if the mouse is on this sprite
+    //     if(
+    //         mouse.x > this.position.x &&
+    //         mouse.x < this.position.x + this.size &&
+    //         mouse.y > this.position.y &&
+    //         mouse.y < this.position.y + this.size
+    //     ) {
+    //         this.color = 'green';
+    //     } else {
+    //         this.color = 'rgba(255, 255, 255, 0.25)';
+    //     }
+    // }
 }
